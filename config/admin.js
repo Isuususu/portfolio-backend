dcf4633ce677bc063d6e533aecc42c4f6714c845
Isuususu <jakubb12@gmail.com>
2023-12-11 12:24:40 +0000

    fix

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,19 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env(
+      "ADMIN_JWT_SECRET",
+      "UXHvaokmfwSWsY42Z2Xmf/aoes979+W1msFZ+IOZ570GnJyFQMoO6Vm7x4mgl/+n"
+    ),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env(
+      "API_TOKEN_SALT",
+      "PdAduBwY2RnkbD9GQy6JpjzscJNoMuuRucW+5Z2OiPuIEGgYNoQyc11OmO6YF0zC"
+    ),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
 });
