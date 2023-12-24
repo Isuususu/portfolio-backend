4db5736093b30f42d1a45ad72ac38425197d4bca
Isuususu <jakubb12@gmail.com>
2023-12-24 17:54:17 +0000

    fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,13 @@ module.exports = [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
           "media-src": [
             "'self'",
             "data:",
